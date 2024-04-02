cd8c91b5f0bfa768dba6f3be699c4d045d0f6dd5
Sobirov3636 <sobirov3636@gmail.com>
2024-04-02 23:51:40 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,27 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
 
 /**  2-SESSIONS **/
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET), // => secretni .env filedan olyapmiz
+    cookie: {
+      maxAge: 1000 * 3600 * 3, // 3 hours  => cookie qancha vaqt saqlanishini belgilayapmiz
+    },
+    store: store, // => store ga biz yuqorida yasagan mongoDBdagi sessions collectionni beryapmiz.
+    resave: true, // => cookieimiz 3 soat mobaynida saqlanar edi. Shu oraliqda user qayta kirsa osha kirgan vaqtidan boshlab yana 3 soatni hisoblaydi. false boladigan bolsa, har kirganida update bolmaydi birinchi kirgan vaqti boyicha hisoblaydi.
+    saveUninitialized: true,
+  })
+);
 /**  3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
