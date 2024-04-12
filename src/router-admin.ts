0c9e747a30d9817767209e4c737a923ce15650cf
Sobirov3636 <sobirov3636@gmail.com>
2024-04-12 16:18:28 +0900

    feat: upload files into server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,18 +2,26 @@ import express from "express";
 const routerAdmin = express.Router();
 import adminController from "./controllers/admin.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 /** Admin */
 routerAdmin.get("/", adminController.goHome);
 routerAdmin.get("/login", adminController.getLogin).post("/login", adminController.processLogin);
-routerAdmin.get("/signup", adminController.getSignup).post("/signup", adminController.processSignup);
+routerAdmin
+  .get("/signup", adminController.getSignup)
+  .post("/signup", makeUploader("members").single("memberImage"), adminController.processSignup);
 routerAdmin.get("/logout", adminController.logout);
 routerAdmin.get("/check-me", adminController.checkAuthSession);
 
 /** Product */
 
 routerAdmin.get("/product/all", adminController.verifyAdmin, productController.getAllProducts);
-routerAdmin.post("/product/create", adminController.verifyAdmin, productController.createNewProduct);
+routerAdmin.post(
+  "/product/create",
+  adminController.verifyAdmin,
+  makeUploader("products").array("productImages", 5),
+  productController.createNewProduct
+);
 routerAdmin.post("/product/:id", adminController.verifyAdmin, productController.updateChosenProduct);
 /** User */
 
