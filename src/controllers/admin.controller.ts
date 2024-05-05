462026fbfb86967a42d80b9e1a5629226674d436
Sobirov3636 <sobirov3636@gmail.com>
2024-05-05 14:40:19 +0900

    fix: modify login page and processLogin logic

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -75,7 +75,7 @@ adminController.processLogin = async (req: AdminRequest, res: Response) => {
   } catch (err) {
     console.log("Error, processLogin", err);
     const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-    res.send(`<script>alert("${message}"); window.location.replace('/admin/signup')</script>`);
+    res.send(`<script>alert("${message}"); window.location.replace('/admin/login')</script>`);
   }
 };
 
